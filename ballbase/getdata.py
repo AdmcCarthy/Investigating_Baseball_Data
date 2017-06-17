13f2dd95c2bc0838b7cd96f79425f01cef200df3
CarthyCraft <a.mccarthy.uk@gmail.com>
2017-06-17 22:17:17 +0200

    feat: Added check to download
    Added if statement to check if directory has alrady been downloaded, unzipped and is present. This is to stop the dataset having to be downloaded everytime.

--- ballbase/getdata.py
+++ ballbase/getdata.py
@@ -8,12 +8,17 @@ def download_file():
     """Download the dataset from a website
     then unzip it.
     """
-
-    # Get baseball data in csv form from website
-    url = "http://seanlahman.com/files/database/baseballdatabank-2017.1.zip"
-    request.urlretrieve(url, filename="../baseballdatabank-2017.1.zip")
-
-    # Unzip the dataset in the folder above the repo.
     os.chdir("..")
-    tfile = tarfile.open("enron_mail_20150507.tgz", "r:gz")
-    tfile.extractall(".")
+    if not os.path.isdir("baseballdatabank-2017.1"):
+
+        # Get baseball data in csv form from website
+        url = "http://seanlahman.com/files/database/baseballdatabank-2017.1.zip"
+        request.urlretrieve(url, filename="../baseballdatabank-2017.1.zip")
+
+        # Unzip the dataset in the folder above the repo.
+        tfile = tarfile.open("enron_mail_20150507.tgz", "r:gz")
+        tfile.extractall(".")
+        print("Data downloaded and unzipped")
+
+    else:
+        print("Data previously downloaded")
