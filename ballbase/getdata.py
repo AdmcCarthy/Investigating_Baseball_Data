e35f15000d7e6dbd5aa47f0f41a4dcae56716fd1
Adam McCarthy <a.mccarthy.uk@googlemail.com>
2017-06-18 19:47:46 +0200

    style: Altered getdata.py
    Added more print statments to give information about what is occuring to getdata.py

--- ballbase/getdata.py
+++ ballbase/getdata.py
@@ -8,17 +8,21 @@ def download_file():
     """Download the dataset from a website
     then unzip it.
     """
+
     os.chdir("..")
     if not os.path.isdir("baseballdatabank-2017.1"):
 
+        print("Starting download")
         # Get baseball data in csv form from website
         url = "http://seanlahman.com/files/database/baseballdatabank-2017.1.zip"
         request.urlretrieve(url, filename="../baseballdatabank-2017.1.zip")
 
         # Unzip the dataset in the folder above the repo.
-        tfile = tarfile.open("enron_mail_20150507.tgz", "r:gz")
+        tfile = tarfile.open("baseballdatabank-2017.1.zip", "r:gz")
         tfile.extractall(".")
         print("Data downloaded and unzipped")
+        print("This database is copyright 1996-2017 by Sean Lahman.")
+        print("Data licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.")
 
     else:
         print("Data previously downloaded")
