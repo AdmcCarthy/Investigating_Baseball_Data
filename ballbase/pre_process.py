fa153dec5318fea7e6e48856b708696ee26caa69
Adam McCarthy <a.mccarthy.uk@googlemail.com>
2017-06-20 22:31:35 +0200

    feat: Finished p_salaries
    Completed p_salaries to deliver statistics about each players career salary

--- ballbase/pre_process.py
+++ ballbase/pre_process.py
@@ -19,54 +19,35 @@ def standarize_column(column):
     return value
 
 
-def p_salaries(folder):
-    """Function to process through the salaries
-    csv file.
+def gpby_tranpose_stats(df, group, column):
+    """Groupby a column (group), then tranpose
+    a set of values (column) on a dataframe (df).
 
-    The objective is to find the players salary per year
-    and standardize this so it is comparable to all salaries
-    that year (seeing as salaries amounts change from 1985 to 2016)
+    This will create a series of columns for all
+    the values of (column).
 
-    Based on this standardization each players max year, min year
-    and average over all years played will be stored as new columns.
+    Following this calulcate statistics along this
+    row (axis = 'columns').
 
-    As well as ranking players their difference compared to the mean will be stored
-    for max year, min year and average of all years played.
-
-    This should allow for relative comparisson of each player
-    even with changing salaries over time.
-
-    Maximum salary value will also be recorded as an additional
-    column.
-
-    Takes folder as a positional argument specifying
-    the folder where the baseballdatabank folder is stored.
-
-    Returns a dataframe fitting the conditions above.
+    Combine results into a dataframe and return.
     """
 
-    # Get file location
-    directory = folder
-    file_loc = os.path.join(directory, "baseballdatabank-2017.1", "core", "Salaries.csv")
+    # Groupby and transpose a column of values according to column used to groupby
+    group_column = df.sort_values(group).groupby(group)[column].apply(lambda df: df.reset_index(drop=True)).unstack()
 
-    # Use pandas to convert the csv to a dataframe.
-    df_salary = pd.read_csv(file_loc)
+    # Calculate statistics for all values in a row
+    group_mean = group_column.mean(axis = 'columns')
+    group_mean.name = ('mean_'+ str(column))
+    group_max = group_column.max(axis = 'columns')
+    group_max.name = ('max_' + str(column))
+    group_min = group_column.min(axis = 'columns')
+    group_min.name = ('min_' + str(column))
 
-    # Stanrdize the salary for each year
-    yearly_mean = df_salary.groupby('yearID')['salary'].apply(standarize_column)
+    # Combine into a dataframe for output
+    df_group = pd.concat([group_mean, group_max, group_min], axis=1)
 
-    # Merge the new column onto the dataframe
-    df_salary_2 = pd.concat([df_salary, yearly_mean], axis=1)
+    return df_group
 
-    # Groupy playerid and then transpose all salary values
-    # using a rather ugly long line of code.
-    player_salaries = df_salary.sort_values('playerID').groupby('playerID')['salary'].apply(lambda df: df.reset_index(drop=True)).unstack()
-
-    player_mean = player_salaries.mean(axis='columns')
-
-    print('')
-    print('Processed Salary data')
-    print(player_mean)
 
 def p_hallfame(folder):
     """Function to process through the hall of fame
@@ -168,3 +149,61 @@ def p_awards(folder):
     print(df_times_awards.head())
 
     return df_times_awards
+
+
+def p_salaries(folder):
+    """Function to process through the salaries
+    csv file.
+
+    The objective is to find the players salary per year
+    and standardize this so it is comparable to all salaries
+    that year (seeing as salaries amounts change from 1985 to 2016)
+
+    Based on this standardization each players max year, min year
+    and average over all years played will be stored as new columns.
+
+    The salary value will be stored
+    for max year, min year and average of all years played.
+
+    This should allow for relative comparisson of each player
+    even with changing salaries over time.
+
+    Maximum salary value will also be recorded as an additional
+    column.
+
+    Takes folder as a positional argument specifying
+    the folder where the baseballdatabank folder is stored.
+
+    Returns a dataframe fitting the conditions above.
+    """
+
+    # Get file location
+    directory = folder
+    file_loc = os.path.join(directory, "baseballdatabank-2017.1", "core", "Salaries.csv")
+
+    # Use pandas to convert the csv to a dataframe.
+    df_salary = pd.read_csv(file_loc)
+
+    # Groupy playerid and then transpose all salary values
+    # get mean, min and max and all players and return.
+    player_salary = gpby_tranpose_stats(df_salary, 'playerID', 'salary')
+
+    # Stanrdize the salary for each year
+    yearly_mean = df_salary.groupby('yearID')['salary'].apply(standarize_column)
+    yearly_mean.name = 'salary_standardized_annualy'
+
+    # Merge the new column onto the dataframe
+    df_salary_2 = pd.concat([df_salary, yearly_mean], axis=1)
+
+    # Groupy player id and then tranpose all standardized salary values
+    # get mean, min and max and all players and return.
+    player_std_salary = gpby_tranpose_stats(df_salary_2, 'playerID', 'salary_standardized_annualy')
+
+    # Concatenate the two dataframes for each player
+    df_player_salary_stats = pd.concat([player_salary, player_std_salary], axis=1)
+
+    print('')
+    print('Processed Salary data')
+    print(df_player_salary_stats.head())
+
+    return df_player_salary_stats
