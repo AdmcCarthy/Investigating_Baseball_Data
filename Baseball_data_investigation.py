116e4c728e402f0d31d86634f4563130dbea8760
Adam McCarthy <a.mccarthy.uk@googlemail.com>
2017-06-21 22:05:08 +0200

    feat: Completed master_merge
    Added master_merge as the output in Baseball_data_investigations.
    
    This will be the output of pre-processing all individual tables

--- Baseball_data_investigation.py
+++ Baseball_data_investigation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import os
+import pandas as pd
 from ballbase import (
     getdata,
     pre_process
@@ -17,19 +18,19 @@ def main():
     getdata.download_file(skip_this=True)
 
     # Process hall of fame file to get all inducted members.
-    # df_hallfame = pre_process.p_hallfame(directory)
+    df_hallfame = pre_process.p_hallfame(directory)
 
     # Process AllstarFull file to find the number of times a
     # player has been in an all stars game.
-    # df_allstar = pre_process.p_allstar(directory)
+    df_allstar = pre_process.p_allstar(directory)
 
     # Process AwardsPlayers file to find the number of times
     # a player has recieved an award.
-    # df_awards = pre_process.p_awards(directory)
+    df_awards = pre_process.p_awards(directory)
 
     # Process Salaries file to calculate a number of attributes
     # based on this information.
-    # slaray_df = pre_process.p_salaries(directory)
+    df_salary = pre_process.p_salaries(directory)
 
     # Process CollegePlaying file to select a single college
     # for each player (based on most attended).
@@ -38,10 +39,22 @@ def main():
     # from Schools.csv
     df_college_location = pre_process.p_college_loc(directory)
 
+    # Process Master.csv file
     df_master = pre_process.p_master(directory)
 
+    table_list = [
+        df_hallfame,
+        df_allstar,
+        df_awards,
+        df_salary,
+        df_college_location,
+        df_master
+        ]
+
     # Merge all tables together with master.csv
-    master_merge = pd.concat
+    master_merge = pd.concat(table_list, axis=1)
+
+    return master_merge
 
 if __name__ == '__main__':
     main()
