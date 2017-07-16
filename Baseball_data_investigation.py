b439bd4494330329cb72fd7fa051cd940a6d8ccc
Adam James McCarthy <a.mccarthy.uk@gmail.com>
2017-07-16 14:03:56 +0200

    style: Fit script to pep 8 style
    Including adding script description

--- Baseball_data_investigation.py
+++ Baseball_data_investigation.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+    Baseball data investigation
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+    Script to run a data processing pipeline
+    to prepare the base ball database.
+
+    Includes downloading the dataset.
+    Followed by pre-processing individual csv files
+    and calulating new attributes from each of these tables.
+
+    Output of pre-processing will be concatenated into one
+    master data table suitable for exploratory data analysis.
+"""
 from __future__ import print_function
 import os
 import pandas as pd
@@ -9,18 +23,23 @@ from ballbase import (
     auditdata
     )
 
+
 def main():
-    """Process the dataset for analysis
+    """
+    Script to run a data processing pipeline
+    to prepare the base ball database.
     """
 
-    # Reset path
+    # Reset path, ensure it starts within repository
     dir_path = os.path.dirname(os.path.realpath(__file__))
     os.chdir(dir_path)
 
-    os.chdir("..") # Store downloaded data outside of repo
-    getdata.download_file(skip_this=True) # Choose to download
+    os.chdir("..")  # Store downloaded data outside of repo
+    getdata.download_file(skip_this=True)  # Choose to skip download
 
     # Specify data folder location
+    #
+    # Can be manually set if data is stored in a different location.
     directory = os.path.dirname(os.path.abspath(os.path.join(__file__, "..")))
 
     # Process hall of fame file to get all inducted members.
@@ -57,17 +76,21 @@ def main():
         df_college_location
         ]
 
-    # Merge all tables together with df_college_location
-    master_merge = pd.concat(table_list, axis=1, join='inner', join_axes=[df_college_location.index], verify_integrity=True)
-
+    # Concatenate all tables together with df_college_location
+    master_merge = pd.concat(
+                             table_list,
+                             axis=1,
+                             join='inner',
+                             join_axes=[df_college_location.index],
+                             verify_integrity=True
+                             )
     print('master_merge is ready')
 
     master_merge = auditdata.data_audit(master_merge)
-
     print('data audit complete')
-    temp2 = master_merge["birthYear"].dropna().copy()
-    print("final birth year size", temp2.size)
+
     return master_merge
+
 
 if __name__ == '__main__':
     main()
